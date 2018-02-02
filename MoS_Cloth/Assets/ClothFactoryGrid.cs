d2c42bed922310ee496072a2fa00caf9517e9720
Walle <adde_wahlgren@hotmail.com>
2018-02-02 16:47:36 +0100

    Lagt till så den skapar punkter ut efter det vi skickar in. Fixat horisontella länkar mellan punkterna.

--- MoS_Cloth/Assets/ClothFactoryGrid.cs
+++ MoS_Cloth/Assets/ClothFactoryGrid.cs
@@ -25,7 +25,37 @@ public class ClothFactoryGrid : ClothFactory
 	{
 		points = new List<VeryLett.ClothPoint>();
 		links  = new List<VeryLett.ClothLink>();
+        int numPixelsX = cellsX + 1 ;
+        int numPixelsY = cellsY + 1;
 
-		
-	}
+        // Add points
+       for (int y = 0; y < numPixelsY; y++)
+        {
+            for (int x = 0; x < numPixelsX; x++)
+            {
+                // for each row , jump to the start of the dimensional square and create points with a set distance to the right.
+                points.Add(new VeryLett.ClothPoint(transform.position -Vector3.up * y * (dimY/cellsY) -Vector3.right * (dimX/2) + Vector3.right * x * (dimX/cellsX)));
+               // Debug.Log(dimX/cellsX);
+            }
+        }
+
+        // Add links horizontal
+        for (int rowNum = 0; rowNum <= numPixelsY; ++rowNum)
+        {
+            for (int link = 0; link < cellsX; ++link)
+            {
+                links.Add(new VeryLett.ClothLink(points[rowNum * numPixelsX + link], points[rowNum * numPixelsX + link + 1]));
+            }
+        }
+
+        // Add links vertical
+        for (int colNum = 0; colNum <= numPixelsX; ++colNum)
+        {
+            for (int link = 0; link < cellsY; ++link)
+            {
+                links.Add(new VeryLett.ClothLink(points[colNum * numPixelsY + link * numPixelsX], points[colNum * numPixelsY + link * numPixelsX + numPixelsX])); //Funkar ej
+                //links.Add(new VeryLett.ClothLink(points[0], points[4]));  <-- Det funkar inte heller
+            }
+        }
+    }
 }
