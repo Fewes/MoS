04e5b3ca7a5555b582b074d1964d25d0eeea3a41
unknown <DennyLindberg@users.noreply.github.com>
2018-03-01 12:59:37 +0100

    Gondola is now walking in the demo scene

---
+++ MoS_Cloth/Assets/Meshes/GondolaMoveScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GondolaMoveScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += transform.forward * 1.75f * Time.deltaTime;
+        transform.Rotate(Vector3.up, 45.0f * Time.deltaTime);
+
+    }
+}
